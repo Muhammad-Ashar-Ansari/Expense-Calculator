3d93582549038925ed96733a3a572b3e7f5c8d5b
Muhammad-Ashar-Ansari <144896935+Muhammad-Ashar-Ansari@users.noreply.github.com>
2025-07-02 17:12:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// console.log('hello');
 let previous_balance = document.getElementById('previous-balance');
 let fri_donation = document.getElementById('fri-donation');
 let rent = document.getElementById('rent');
@@ -7,7 +6,7 @@ let total_result = document.getElementById('total-result');
 
 document.getElementById('submit').addEventListener('click', function(event) {
     event.preventDefault(); 
-
+    
     let pb = parseFloat(previous_balance.value) || 0;
     let fd = parseFloat(fri_donation.value) || 0;
     let r = parseFloat(rent.value) || 0;
@@ -15,5 +14,25 @@ document.getElementById('submit').addEventListener('click', function(event) {
 
     let result = pb + fd + r + od;
 
-    total_result.textContent = result;
+    total_result.textContent = `${result} Rs`;
 });
+
+let required = document.getElementById('pdf_btn').addEventListener('click', function(event){
+    if(pb === '' || fd === '' || r === '' || od === ''){
+        alert('Please fill all the information!');
+    }
+});
+
+function downloadPDF() {
+  const element = document.getElementById("pdf-section");
+
+  const opt = {
+    margin: 0.5,
+    filename: 'donation-summary.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+  };
+
+  html2pdf().set(opt).from(element).save();
+}
